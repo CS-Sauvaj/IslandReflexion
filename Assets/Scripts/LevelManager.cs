6ea2068c804bdd90e29aae0446d969cc25b2fcb1
CS-Sauvaj <hugosimonyjungo@aol.fr>
2020-05-22 20:02:33 +0200

    Niveau 1 quasi fini

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,11 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     static int idGroup = 1;
 
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+    public Button button10;
+    public Button button11;
+    public Button button12;
+    public Button button13;
+    public Button button14;
+    public Button button15;
+    public Button button16;
+    public Button button17;
+    public Button button18;
+    public Button button19;
+    public Button button20;
+    public Button button21;
+    public Button button22;
+    public Button button23;
+    public Button button24;
+
+    public Button groupe1;
+    public Button groupe2;
+    public Button groupe3;
+    public Button groupe4;
 
     public void Grp1()
     {
@@ -29,7 +59,273 @@ public class LevelManager : MonoBehaviour
         idGroup = 4;
     }
 
+    public void Button1()
+    {
+        Debug.Log("Bouton 1 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(0, 0))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
 
+    public void Button2()
+    {
+        Debug.Log("Bouton 2 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(1, 0))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+
+    public void Button3()
+    {
+        Debug.Log("Bouton 3 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(2, 0))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+
+    public void Button4()
+    {
+        Debug.Log("Bouton 4 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(3, 0))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button5()
+    {
+        Debug.Log("Bouton 5 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(4, 0))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button6()
+    {
+        Debug.Log("Bouton 6 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(5, 0))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button7()
+    {
+        Debug.Log("Bouton 7 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(0, 1))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button8()
+    {
+        Debug.Log("Bouton 8 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(1, 1))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button9()
+    {
+        Debug.Log("Bouton 9 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(2, 1))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button10()
+    {
+        Debug.Log("Bouton 10 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(3, 1))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button11()
+    {
+        Debug.Log("Bouton 11 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(4, 1))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button12()
+    {
+        Debug.Log("Bouton 12 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(5, 1))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button13()
+    {
+        Debug.Log("Bouton 13 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(0, 2))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button14()
+    {
+        Debug.Log("Bouton 14 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(1, 2))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button15()
+    {
+        Debug.Log("Bouton 15 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(2, 2))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button16()
+    {
+        Debug.Log("Bouton 16 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(3, 2))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button17()
+    {
+        Debug.Log("Bouton 17 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(4, 2))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button18()
+    {
+        Debug.Log("Bouton 18 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(5, 2))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button19()
+    {
+        Debug.Log("Bouton 19 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(0, 3))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button20()
+    {
+        Debug.Log("Bouton 20 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(1, 3))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button21()
+    {
+        Debug.Log("Bouton 21 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(2, 3))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button22()
+    {
+        Debug.Log("Bouton 22 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(3, 3))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button23()
+    {
+        Debug.Log("Bouton 23 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(4, 3))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
+    public void Button24()
+    {
+        Debug.Log("Bouton 24 cliqué");
+        Text text = button1.GetComponent<Text>();
+        text.text = (idGroup.ToString());
+        if (setGroup(5, 3))
+        {
+            Debug.Log("Win");
+            Win();
+        }
+    }
 
     // BLEU = 1
     // ROUGE = 2
@@ -53,11 +349,18 @@ public class LevelManager : MonoBehaviour
 
 
 
-    static void setGroup(int i, int j)
+    static bool setGroup(int j, int i)
     {
-        // Faire la partie graphique
         if (puzzle1[i, j, 1] != idGroup)
         {
+            if (puzzle1[i, j, 1] == 1)
+                group1.Remove(puzzle1[i, j, 0].ToString());
+            else if (puzzle1[i, j, 1] == 2)
+                group2.Remove(puzzle1[i, j, 0].ToString());
+            else if (puzzle1[i, j, 1] == 3)
+                group3.Remove(puzzle1[i, j, 0].ToString());
+            else if (puzzle1[i, j, 1] == 4)
+                group4.Remove(puzzle1[i, j, 0].ToString());
             if (idGroup == 1)
                 group1.Add(puzzle1[i, j, 0].ToString());
             else if (idGroup == 2)
@@ -68,6 +371,7 @@ public class LevelManager : MonoBehaviour
                 group4.Add(puzzle1[i, j, 0].ToString());
             puzzle1[i, j, 1] = idGroup;
         }
+        return checkPuzzle();
     }
 
     static bool checkPuzzle()
@@ -134,5 +438,10 @@ public class LevelManager : MonoBehaviour
         }
         else
             return false;
+    }
+
+    public void Win()
+    {
+
     }
 }
